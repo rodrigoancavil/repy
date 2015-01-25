2ee748a455cb48ad7f75674aafba45dcdcc9ab59
Rodrigo Ancavil <rancavil@gmail.com>
2015-01-25 00:51:00 -0300

    second commit for parsehtml.py

--- parsehtml.py
+++ parsehtml.py
@@ -5,9 +5,10 @@ N = int(raw_input())
 if N>=1 and N <=100:
 	for i in range(0,N):
 		W = raw_input()
-		fragment = re.findall('<\w+\s+\w+=".*">|<\w+>',W)
+		fragment = re.split('<',W)
+		print
 		if len(fragment) > 10000:
 			break
 		else:
 			for tag in fragment:
-				print tag
+				print re.findall('\w+',tag)	
